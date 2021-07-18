ccdfe478aabce7784e0bdc9671bc269ccf57b898
vijay kumar vemana <vijaykumarvemana18@gmail.com>
2021-07-18 21:16:29 +0200

    updated

--- script.js
+++ script.js
@@ -4,9 +4,27 @@
   const todoList = document.querySelector('.todo-list')
 
 //EVENT LISTENERS
-document.addEventListener('click', addTodo)
 
 //FUNCTIONS
-const addTodo = function (){
+const addTodo = function (event){
+    event.preventDefault()
+    const todoDiv = document.createElement('Div')
+    todoDiv.classList.add('todo')
+    //console.log(todoDiv)
+    const todoLi = document.createElement('li')
+    const inputValue = todoInput.value
+    todoLi.innerText =inputValue
+    todoLi.classList.add('todo-item')
+    //console.log(todoLi)
+    
+    todoDiv.appendChild(todoLi)
+    const trashButton = document.createElement("button");
+    trashButton.innerHTML = `<i class="fas fa-trash"></i>`;
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
+   
+    todoList.appendChild(todoDiv)
+} 
+//EVENT LISTENERS
 
-}
+todoButton.addEventListener('click', addTodo)
